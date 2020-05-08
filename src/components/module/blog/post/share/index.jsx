472444b365dc0d60e54590f5384c2ab61b8ba633
Gokul prabhu <gokulprabhu.droid@gmail.com>
2020-05-09 00:27:38 +0530

    Path fix in prod build

--- src/components/module/blog/post/share/index.jsx
+++ src/components/module/blog/post/share/index.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { LinkedinShareButton, LinkedinIcon, TwitterShareButton , EmailShareButton, EmailIcon, TwitterIcon }from "react-share";
 import './styles.scss';
 
-const Share = (props) => {
-  const title = props.title;
-  return (
-    <div className="share-container">
-      <p><small>Share this article on:</small></p>
-      <LinkedinShareButton url={window.location.href} title={title}> 
-        <LinkedinIcon size={50} round/>
-      </LinkedinShareButton>
-      <TwitterShareButton url={window.location.href} title={title}>
-        <TwitterIcon size={50} round/>
-      </TwitterShareButton>
-      <EmailShareButton url={window.location.href} subject={title}> 
-        <EmailIcon size={50} round/>
-      </EmailShareButton>
-    </div>
-  )
+class Share extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ""
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ url: window.location.href});
+  }
+
+  render() {
+    const title = this.props.title;
+    return (
+      <div className="share-container">
+        <p><small>Share this article on:</small></p>
+        <LinkedinShareButton url={this.state.url} title={title}> 
+          <LinkedinIcon size={50} round/>
+        </LinkedinShareButton>
+        <TwitterShareButton url={this.state.url} title={title}>
+          <TwitterIcon size={50} round/>
+        </TwitterShareButton>
+        <EmailShareButton url={this.state.url} subject={title}> 
+          <EmailIcon size={50} round/>
+        </EmailShareButton>
+      </div>
+    );
+  }
 }
+
 
 export default Share;
